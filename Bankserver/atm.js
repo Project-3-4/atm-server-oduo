f08d5dc179c8d10f871f8c9bdc450c5434de474c
RJ <90836237+HR-RJ@users.noreply.github.com>
2022-06-06 18:12:57 +0200

    ATM almost there edition

--- Bankserver/atm.js
+++ Bankserver/atm.js
@@ -5,29 +5,52 @@ const http = require('http');
 const https = require('https');
 //const mysql = require('mysql');
 
+const data = JSON.stringify({
+    head: {
+        'fromCtry': 'GR',
+        'fromBank': 'ODUO',
+        'toCtry':   'GR',
+        'toBank':   'ODUO'
+    },
+    body: {
+        'acctNo' : '\'GRODUO0000135700\'',
+        'pin': '\'1234\''//,
+        // 'amount' : '300'
+    }
+});
 
 const options = {
-  hostname: 'localhost:8443/balance',
-  port: 443,
-  path: '/balance',
-  method: 'POST'
+    hostname: '145.24.222.225',
+    port: 8443,
+    path: '/balance',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+    }
 };
 
-const req = https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-  console.log('body', res.body );
+const req = http.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+    if (res.statusCode == 200) {
+        console.log("we got \'em")
+    }
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
+    res.on('data', (d) => {
+        process.stdout.write(d);
+        const resData = JSON.parse(d);
+        console.log(resData.body.body);
+        console.log(resData.body.balance);
+    });
 });
 
 req.on('error', (e) => {
-  console.error(e);
+    console.error(e);
 });
+req.write(data);
 req.end();
 
 http.createServer(req).listen(443, function(){
-    console.log('listening on port 8443');
+    console.log('listening on port 443');
 })
